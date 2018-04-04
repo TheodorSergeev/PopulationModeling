4e91012ca7ddb308dedcf38743a64118321e034b
Fedor Sergeev <8850050+TheodorSergeev@users.noreply.github.com>
2018-04-05 00:00:55 +0300

    Add files via upload
    Update: rewrote bits of code - should look slightly better now. Solved some issues with inaccurate grid lines and rectangles.

--- Window-Application/main.cpp
+++ Window-Application/main.cpp
@@ -4,10 +4,14 @@
 #include <wx/txtstrm.h>
 #include <wx/sstream.h>
 #include <wx/string.h>
-#include <sstream>
-#include <string>
+#include <wx/gdicmn.h> // colours database
 
-class WinApplication: public wxApp
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+class CWinApplication: public wxApp
 {
 
 public:
@@ -15,41 +19,59 @@ public:
 
 };
 
-//for crossreferences
-class Draw;
-class MyWin;
+class CFieldDrawPane;  //for crossreferences between CDraw and CCMainWindow
+class CMainWindow;
 
-const int WINDOW_HEIGHT = 300;
-const int WINDOW_WIDTH  = 400;
 
-class MyWin: public wxFrame
+const int WINDOW_HEIGHT = 600;
+const int WINDOW_WIDTH  = 800;
+
+// field positioning
+const double BORDER_X_LEFT  = 50;
+const double BORDER_Y_TOP   = 50;
+const double FIELD_X_SIZE   = std::min(WINDOW_HEIGHT - 2 * BORDER_Y_TOP, WINDOW_WIDTH - 2 * BORDER_X_LEFT);
+const double FIELD_Y_SIZE   = FIELD_X_SIZE;
+
+class CMainWindow: public wxFrame
 {
+
+private:
 
     wxMenuBar*  headmenu_bar;      // полоса для меню
     wxMenu*     headmenu_item;     // меню в заголовке окна
     wxMenuItem* headmenu_but_quit; // кнопка в заголовке окна
 
-    Draw*    dp;        // Указатель на объект Draw
-    wxPanel* m_pan;     // Это в "подвале" окошка
+    CFieldDrawPane* fdraw_pane;    // Указатель на объект CFieldDrawPane
+    wxPanel* base_panel;           // Это в "подвале" окошка
 
  public:
 
-    MyWin(const wxString& title);       //  конструктор
-    void OnQuit(wxCommandEvent& event); // Функция закрытия окна
+    CMainWindow(const wxString& title);
+    void OnQuit(wxCommandEvent& event);
 
 };
 
-// класс для рисования
-// наследник wxPanel
-class Draw: public wxPanel
+class CFieldDrawPane: public wxPanel
 {
 
-    MyWin *mn; // указатель на окно-родителя
+private:
+
+    int cells_num_x;
+    int cells_num_y;
+    int cell_x_size;
+    int cell_y_size;
+
+    wxSize canvas_size;
+
+    CMainWindow *mn; // указатель на окно-родителя
 
 public:
 
-    Draw(wxPanel* parent, MyWin* main);
+    CFieldDrawPane(wxPanel* parent, CMainWindow* main);
     void OnPaint(wxPaintEvent & event);
+
+    void DrawGrid(wxDC& dc);
+    void DrawCell(int x, int y, int col_num, wxDC& dc);
 
 };
 
@@ -57,17 +79,17 @@ public:
 const int ID_MENU_LOAD = 1002; // загрузка
 const int ID_MENU_EDIT = 1003; // редактирование
 
+std::vector <wxColour> cell_colors;
+
 // Конструктор фрейма
-MyWin::MyWin(const wxString& title):wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(WINDOW_WIDTH, WINDOW_HEIGHT),
+CMainWindow::CMainWindow(const wxString& title):wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(WINDOW_WIDTH, WINDOW_HEIGHT),
                                             (wxDEFAULT_FRAME_STYLE & ~wxRESIZE_BORDER))
 {
 
     headmenu_bar  = new wxMenuBar; // создали полоску для менюшки
     headmenu_item = new wxMenu;    // создали менюшку
 
-    // закинули менюшку на полоску
-    // пока не работает (для "красоты")
-    headmenu_item->Append(wxID_ANY, wxT("&todo"));
+    headmenu_item->Append(wxID_ANY, wxT("&todo")); // закинули менюшку на полоску пока не работает (для "красоты")
     headmenu_item->AppendSeparator();
 
     // Для всех пунктов меню указывем идентификатор чтобы связать обработчик событие с конкретным элементом
@@ -75,38 +97,39 @@ MyWin::MyWin(const wxString& title):wxFrame(NULL, wxID_ANY, title, wxDefaultPosi
     headmenu_item->Append(headmenu_but_quit);
     headmenu_bar->Append(headmenu_item, wxT("&File")); // добавили оба пункта меню на полску меню
     SetMenuBar(headmenu_bar);  // установили полоску в окно
-    Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MyWin::OnQuit)); // подключили менюшку exit
+    Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(CMainWindow::OnQuit)); // подключили менюшку exit
 
-    m_pan = new wxPanel(this, wxID_ANY); // создание панельки для текста, кнопок и рисовалки
-    dp    = new Draw(m_pan, this);       // тоже панель, но наша, помещаем ее на панель m_pan и задаем указатель на главный фрейм
+    base_panel = new wxPanel(this, wxID_ANY); // создание панельки для текста, кнопок и рисовалки
+    fdraw_pane = new CFieldDrawPane(base_panel, this);       // тоже панель, но наша, помещаем ее на панель base_panel и задаем указатель на главный фрейм
 
 };
 
-void MyWin::OnQuit(wxCommandEvent& event)
+void CMainWindow::OnQuit(wxCommandEvent& event)
 {
 
     Close(true);
 
 };
 
-Draw::Draw(wxPanel *parent, MyWin *fr):wxPanel(parent, -1, wxPoint(47.5, 47.5), wxSize(WINDOW_WIDTH - 47.5*2, WINDOW_HEIGHT - 47.5*2), wxBORDER_SUNKEN)
+CFieldDrawPane::CFieldDrawPane(wxPanel *parent, CMainWindow *fr):wxPanel(parent, -1, wxPoint(BORDER_X_LEFT, BORDER_Y_TOP), wxSize(FIELD_X_SIZE, FIELD_Y_SIZE), wxBORDER_NONE)
 {
 
-    Connect(wxEVT_PAINT,wxPaintEventHandler(Draw::OnPaint)); // подключили панель к событиям рисования
+    Connect(wxEVT_PAINT,wxPaintEventHandler(CFieldDrawPane::OnPaint)); // подключили панель к событиям рисования
     mn = fr;
 
+    // constant
+    cells_num_x = 20;
+    cells_num_y = 20;
+
+    cell_colors.push_back(wxT("GOLD"));
+    cell_colors.push_back(wxT("FIREBRICK"));
+    cell_colors.push_back(wxT("NAVY"));
+    cell_colors.push_back(wxT("FOREST GREEN"));
+    cell_colors.push_back(wxT("ORCHID"));
+    cell_colors.push_back(wxT("CYAN"));
+
 };
 
-void DrRec(wxPoint a, wxPoint b, wxDC * dc)
-{
-
-    dc->DrawRectangle(a, wxSize(abs(a.x - b.x), abs(a.y - b.y)));
-
-};
-
-// Функция OnPaint() сработает при любом рисовании окна:
-// первое рисование, сложил-разложили
-// или вызов функции Refresh()
 
 void Print(wxPaintDC& dc, std::string str_)
 {
@@ -120,49 +143,65 @@ void Print(wxPaintDC& dc, std::string str_)
 
 }
 
-void Draw::OnPaint(wxPaintEvent& event)
+void CFieldDrawPane::DrawGrid(wxDC& dc)
 {
 
-    wxPaintDC dc(this);                        // положили планшет wxPaintDC на нашу панель
-
-    wxSize canvas_size = dc.GetSize();
-    //canvas_size.x = 300;
-    //canvas_size.y = 200;
-
-    //std::string str_;
-    //str_ += canvas_size.x + " " + canvas_size.y;
-    //Print(dc, str_);
-
-
-
-    const int CELLS_X = 10;
-    const int CELLS_Y = 10;
-
-    double cell_x_size = canvas_size.x / CELLS_X;
-    double cell_y_size = canvas_size.y / CELLS_Y;
-
-    for(double x = 0; x <= canvas_size.x; x += cell_x_size)
+    for(int x = 0; x <= canvas_size.x; x += cell_x_size)
     {
 
-         dc.DrawLine(x, 0, x, canvas_size.y);
+//         if(x != 0)
+            dc.DrawLine(x, 0, x, canvas_size.y);
 
     }
 
-    for(double y = 0; y <= canvas_size.y; y += cell_y_size)
+    for(int y = 0; y <= canvas_size.y; y += cell_y_size)
     {
 
+//        if(y != 0)
          dc.DrawLine(0, y, canvas_size.x, y);
 
     }
 
+    dc.DrawLine(canvas_size.x - 1, 0, canvas_size.x - 1, canvas_size.y);
+    dc.DrawLine(0, canvas_size.y - 1, canvas_size.x, canvas_size.y - 1);
+
+}
+
+void CFieldDrawPane::DrawCell(int x_pos, int y_pos, int col_num, wxDC& dc)
+{
+
+    dc.SetBrush(wxBrush(cell_colors[col_num]));
+    dc.SetPen(wxPen(*wxRED, 0, wxTRANSPARENT));
+    dc.DrawRectangle(cell_x_size * x_pos + 1, cell_y_size * y_pos + 1, cell_x_size - 1, cell_y_size - 1);
+
+}
+
+void CFieldDrawPane::OnPaint(wxPaintEvent& event)
+{
+
+
+    wxPaintDC dc(this); // положили планшет wxPaintDC на нашу панель
+
+    canvas_size = dc.GetSize();
+    cell_x_size = std::floor(canvas_size.x / cells_num_x);
+    cell_y_size = std::floor(canvas_size.y / cells_num_y);
+
+    DrawGrid(dc);
+
     wxPoint a(0, 0);
     wxPoint b(20, 20);
 
-    dc.SetBrush(wxBrush(wxColour(255, 0, 0))); // Установили цвет заливки
-    DrRec(a, b, &dc);                  // передали свободной функции координаты и указатель на планшет
+    DrawCell(3, 3, 0, dc);
+    DrawCell(3, 4, 1, dc);
+    DrawCell(9, 9, 2, dc);
+    DrawCell(0, 9, 3, dc);
+    DrawCell(0, 0, 4, dc);
+    DrawCell(7, 8, 5, dc);
+
 
     std::stringstream str;
     str << canvas_size.x << " " << canvas_size.y << "\n";
+    str << cell_x_size << " " << cell_y_size << "\n";
     wxFont font(20, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false);
     dc.SetFont(font);
     dc.SetTextForeground(wxColour(255, 102, 0));
@@ -171,12 +210,12 @@ void Draw::OnPaint(wxPaintEvent& event)
 };
 
 
-IMPLEMENT_APP(WinApplication)
+IMPLEMENT_APP(CWinApplication)
 
-bool WinApplication::OnInit()
+bool CWinApplication::OnInit()
 {
 
-    MyWin* wind = new MyWin(wxT("Моделирование популяций"));
+    CMainWindow* wind = new CMainWindow(wxT("Population modeling"));
     wind->Show(true);
     return true;
 
