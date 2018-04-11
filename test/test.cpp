cf27faa8155a28023279fa01c7e14fd95cd35f82
viviaxenov <viviaxenov@gmail.com>
2018-04-11 23:52:39 +0300

    Testin'

--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,7 @@
 
 
 CCellType CCell::default_cell_type = {0, "your mom", 150, 2, 2};
-const int CFood::DEF_FOOD_VAL = 1;
+const int CFood::DEF_FOOD_VAL = 5;
 
 int main()
 {
@@ -22,22 +22,39 @@ int main()
 	FILE *fp = fopen("dumps.txt", "a");
 	mom->Dump(fp);
 
-	CFood *food_p = new CFood(1);
-	CFood *poison_p = new CFood(-1);
+//	CFood *food_p = new CFood(1);
+//	CFood *food_p2 = new CFood(3);
+//	CFood *poison_p = new CFood(-1);
 //	printf("food at %08X\n", food_p);
 //	printf("poison at %08X\n", poison_p);
 //	printf("bacterium at %08X\n", mom);
 
 	
-	CEnvironment fld(5, 5);
+	CEnvironment fld(6, 6);
 	
 	int x = 2, y = 1, rng = 2;
 	fld.PlantObject(mom, x, y);
 
-	fld.PlantObject(food_p, x - 1, y);
-	fld.PlantObject(poison_p, 1, 0);
+//	fld.PlantObject(food_p, x - 1, y);
+//	fld.PlantObject(food_p2, x - 1, y - 1);
+//	fld.PlantObject(poison_p, 1, 0);
 	fld.PlantObject(eva, 3, 2);
+	fld.DumbSpawnFood();
 	fld.Print();
+	puts("\n");
+
+	fld.Iteration();
+	fld.Print();
+	puts("\n");
+
+	fld.Iteration();
+	fld.Print();
+	puts("\n");
+
+	fld.Iteration();
+	fld.Print();
+	puts("\n");
+	mom->Dump(fp);
 	//int portion = evangelion->Bite(9000);
 	
 
@@ -47,8 +64,8 @@ int main()
 	{
 		printf("%2d %2d %c\n", (*s)[i].x, (*s)[i].y,(*s)[i].type);
 	}
-	coord_t dir = mom->Action(s);
-	printf("YOUR MOM MOVES (%2d,%2d)\n", std::get<1>(dir), std::get<0>(dir));
+	coord_t dir = mom->Direction(s);
+	printf("YOUR MOM MOVES (%2d,%2d)\n", std::get<0>(dir), std::get<1>(dir));
 	s->clear();
 	delete s;
 	
